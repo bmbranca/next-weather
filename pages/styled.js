b9883c64efa00c5258abc8f45912f99ddbd14e59
Brian_Branca <brianbranca@foxtrotco.com>
2022-07-05 09:23:38 -0500

    first styling

---
+++ pages/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const Header = styled.div`
+display: flex;
+justify-content: center;
+background-color: blue;
+font-size: 4rem;
+`
+
+export const Input = styled.input`
+max-width: 500px;
+margin: 0 auto;
+`
+
+export const WeatherBanner = styled.div``
